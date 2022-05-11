10b53d0e507e071ac5ade1df9ebc8fcd885a69c2
Godswilly <kalu.agukalu@gmail.com>
2022-05-11 05:16:37 +0100

    Add auth redux reducer

---
+++ src/redux/reducers/authenticate.js
@@ -0,0 +1,21 @@
+import actionTypes from '../constants/actionTypes';
+
+const initialState = {
+	token: '',
+	name: '',
+	status: false,
+};
+
+const authenticateReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		case actionTypes.AUTHENTICATE:
+			return {
+				...payload,
+			};
+
+		default:
+			return state;
+	}
+};
+
+export default authenticateReducer;

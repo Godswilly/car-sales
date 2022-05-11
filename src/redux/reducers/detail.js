694b2b14831eab3b787e401a9bb13bd295d05385
Godswilly <kalu.agukalu@gmail.com>
2022-05-11 05:20:01 +0100

    Add detail redux reducer

---
+++ src/redux/reducers/detail.js
@@ -0,0 +1,16 @@
+import actionTypes from '../constants/actionTypes';
+
+const initialState = {
+	details: [],
+};
+
+const detailReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		case actionTypes.SINGLE_CAR:
+			return { ...state, details: payload };
+		default:
+			return state;
+	}
+};
+
+export default detailReducer;

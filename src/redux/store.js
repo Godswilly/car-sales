5f48864c5aa9ad1de16763a375ca07ec8c857a4b
Godswilly <kalu.agukalu@gmail.com>
2022-05-11 10:33:39 +0100

    Setup create store

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { legacy_createStore as createStore } from 'redux';
+import { persistStore } from 'redux-persist';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+
+const persistor = persistStore(store);
+export { store, persistor };

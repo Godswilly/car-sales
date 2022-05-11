acec4d62f4fba66168f1d700fbb568f5ad7dfbfa
Godswilly <kalu.agukalu@gmail.com>
2022-05-11 04:49:58 +0100

    Update redux constants

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '../styles/App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='app'>
+			<Router>
+				<NavBar />
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route exact path='/Cars' element={<CarList />} />
+					<Route exact path='/signup' element={<SignUp />} />
+					<Route exact path='/login' element={<Login />} />
+					<Route exact path='/logout' element={<Logout />} />
+					<Route exact path='About' element={<About />} />
+					<Route path='*' element={<Error />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;

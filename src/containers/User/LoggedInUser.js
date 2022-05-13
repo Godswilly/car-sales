a75c6f7dc3cde8ef25f0161c7c6a952f6dc38d11
Godswilly <kalu.agukalu@gmail.com>
2022-05-13 10:08:53 +0100

    Implement logged in user

---
+++ src/containers/User/LoggedInUser.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const LoggedInUser = () => {
+  const auth = useSelector((state) => state.authenticate);
+  
+  return (
+    <div>
+      Welcome @
+      {auth.username}
+    </div>
+  );
+};
+
+export default LoggedInUser;
